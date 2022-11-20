ce6a645d17becdd3c37dd8b61801fd490550e02a
Dmitrii <krivelodmitry@gmail.com>
2022-11-20 18:38:49 +0300

    [Add] Practies-1

--- Assets/Scripts/Krivelo/ex2.cs
+++ Assets/Scripts/Krivelo/ex2.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ex-1 : MonoBehaviour
+public class ex2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        const float PI = 3.14f;
+        float circle, radius = 3;
+
+        circle = PI * radius * radius;
+        print($"ploshad kruga = {circle}");
     }
 
     // Update is called once per frame
